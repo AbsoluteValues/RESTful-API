ea27b8ae943427e9240a6a3cf750ade8fd8498a1
Absolute Value <heejun1708@gmail.com>
2024-09-13 13:48:51 +0900

    Init

---
+++ RESTful/src/test/java/AbsoluteValue/RESTful/ResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package AbsoluteValue.RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
