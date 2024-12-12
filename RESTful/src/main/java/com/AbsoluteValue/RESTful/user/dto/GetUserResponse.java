89bc8df511577ac211d8b0356c08fa1c24a3b19d
AbsoluteValues <heejun1708@gmail.com>
2024-12-12 23:52:58 +0900

    변환 방식 변경 준비

--- RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/GetUserResponse.java
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/GetUserResponse.java
@@ -1,12 +1,7 @@
 package com.AbsoluteValue.RESTful.user.dto;
 
-import com.AbsoluteValue.RESTful.user.vo.User;
-
 public record GetUserResponse(
-    String id,
-    String nickname
+        String id,
+        String nickname
 ) {
-    public static GetUserResponse from(User user) {
-        return new GetUserResponse(user.getId(), user.getNickname());
-    }
 }
--- RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/SignUpUserRequest.java
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/SignUpUserRequest.java
@@ -1,17 +1,8 @@
 package com.AbsoluteValue.RESTful.user.dto;
 
-import com.AbsoluteValue.RESTful.user.vo.User;
-
-public record SignUpUserRequest (
-    String id,
-    String password,
-    String nickname
+public record SignUpUserRequest(
+        String id,
+        String password,
+        String nickname
 ) {
-    public User toEntity() {
-        return User.builder()
-                .id(id)
-                .password(password)
-                .nickname(nickname)
-                .build();
-    }
 }
