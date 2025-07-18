acc60fdc4389ba54400ba7320396ba9b74e4fc68
Absolute Value <heejun1708@gmail.com>
2025-07-18 16:52:23 +0900

    spring validation 추가

--- RESTful/src/main/java/com/AbsoluteValue/RESTful/user/controller/UserController.java
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/user/controller/UserController.java
@@ -5,6 +5,8 @@ import com.AbsoluteValue.RESTful.common.success.SuccessResponse;
 import com.AbsoluteValue.RESTful.user.dto.FindUserResponse;
 import com.AbsoluteValue.RESTful.user.dto.SignUpUserRequest;
 import com.AbsoluteValue.RESTful.user.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +24,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
-    public SuccessResponse signUpUser(@RequestBody SignUpUserRequest signUpUserRequest) {
+    public SuccessResponse signUpUser(@RequestBody @Valid SignUpUserRequest signUpUserRequest) {
         userService.signUpUser(signUpUserRequest);
         return new SuccessResponse(SuccessCode.REGISTER_SUCCESS);
     }
 
     @GetMapping("/user/profile/{id}")
-    public SuccessResponse findUser(@PathVariable String id) {
+    public SuccessResponse findUser(@PathVariable @NotBlank String id) {
         FindUserResponse response = userService.findUser(id);
         return new SuccessResponse(SuccessCode.RESOURCE_FOUND, response);
     }
--- RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/SignUpUserRequest.java
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/SignUpUserRequest.java
@@ -1,8 +1,24 @@
 package com.AbsoluteValue.RESTful.user.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+
 public record SignUpUserRequest(
+
+        @NotBlank
+        @DecimalMin(value = "1")
+        @DecimalMax(value = "10")
         String id,
+
+        @NotBlank
+        @DecimalMin(value = "1")
+        @DecimalMax(value = "500")
         String password,
+
+        @NotBlank
+        @DecimalMin(value = "1")
+        @DecimalMax(value = "10")
         String nickname
 ) {
 }
