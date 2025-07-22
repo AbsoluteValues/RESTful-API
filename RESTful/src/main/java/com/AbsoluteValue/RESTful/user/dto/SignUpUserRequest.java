ad1050814624e0a42dcfeab69769368e0971edbb
Absolute Value <heejun1708@gmail.com>
2025-07-22 17:03:55 +0900

    dto validation 오류 해결

--- RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/SignUpUserRequest.java
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/user/dto/SignUpUserRequest.java
@@ -1,24 +1,20 @@
 package com.AbsoluteValue.RESTful.user.dto;
 
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record SignUpUserRequest(
 
         @NotBlank
-        @DecimalMin(value = "1")
-        @DecimalMax(value = "10")
+        @Size(min = 1, max = 10)
         String id,
 
         @NotBlank
-        @DecimalMin(value = "1")
-        @DecimalMax(value = "500")
+        @Size(min = 1, max = 500)
         String password,
 
         @NotBlank
-        @DecimalMin(value = "1")
-        @DecimalMax(value = "10")
+        @Size(min = 1, max = 10)
         String nickname
 ) {
 }
