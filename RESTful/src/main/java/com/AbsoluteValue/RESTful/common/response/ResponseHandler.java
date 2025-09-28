04c6c81d02ea45712cfe3eaf64f36fb9c4ef12cb
Absolute Value <heejun1708@gmail.com>
2025-09-29 01:38:51 +0900

    ResponseEntity 공통 응답 헨들러

---
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/common/response/ResponseHandler.java
@@ -0,0 +1,40 @@
+package com.AbsoluteValue.RESTful.common.response;
+
+import com.AbsoluteValue.RESTful.common.exception.ErrorResponse;
+import com.AbsoluteValue.RESTful.common.response.code.Code;
+import com.AbsoluteValue.RESTful.common.success.SuccessResponse;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
+
+public class ResponseHandler {
+
+    public static ResponseEntity<SuccessResponse> success(Code code) {
+        SuccessResponse response = new SuccessResponse(code, null);
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(response);
+    }
+
+    public static ResponseEntity<SuccessResponse> success(Code code, Object data) {
+        SuccessResponse response = new SuccessResponse(code, data);
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(response);
+    }
+
+    public static ResponseEntity<ErrorResponse> error(Code code, WebRequest request) {
+        ErrorResponse response = new ErrorResponse(code, request, null);
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(response);
+    }
+
+    public static ResponseEntity<ErrorResponse> error(Code code, WebRequest request,
+                                                      Map<String, Object> additionalData) {
+        ErrorResponse response = new ErrorResponse(code, request, additionalData);
+        return ResponseEntity
+                .status(code.getStatus())
+                .body(response);
+    }
+}
