5dd6ca4a73784d75a237b093231241f81f25454f
Absolute Value <heejun1708@gmail.com>
2025-10-16 16:14:04 +0900

    swagger 적용

--- RESTful/src/main/java/com/AbsoluteValue/RESTful/user/controller/UserController.java
+++ RESTful/src/main/java/com/AbsoluteValue/RESTful/user/controller/UserController.java
@@ -6,6 +6,9 @@ import com.AbsoluteValue.RESTful.common.response.handler.ResponseHandler;
 import com.AbsoluteValue.RESTful.user.dto.FindUserResponse;
 import com.AbsoluteValue.RESTful.user.dto.SignUpUserRequest;
 import com.AbsoluteValue.RESTful.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import java.util.List;
@@ -26,18 +29,36 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/user")
+    @Operation(summary = "회원 가입", description = "신규 회원이 가입할 수 있도록 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", ref = "Created"),
+            @ApiResponse(responseCode = "400", ref = "BadRequest"),
+            @ApiResponse(responseCode = "500", ref = "InternalServerError")
+    })
     public ResponseEntity<SuccessResponse> signUpUser(@RequestBody @Valid SignUpUserRequest signUpUserRequest) {
         userService.signUpUser(signUpUserRequest);
         return ResponseHandler.success(SuccessCode.REGISTER_SUCCESS);
     }
 
     @GetMapping("/user/profile/{id}")
+    @Operation(summary = "회원 정보 단건 조회", description = "특정 회원의 정보를 조회할 수 있도록 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", ref = "Ok"),
+            @ApiResponse(responseCode = "400", ref = "BadRequest"),
+            @ApiResponse(responseCode = "500", ref = "InternalServerError")
+    })
     public ResponseEntity<SuccessResponse> findUser(@PathVariable @NotBlank String id) {
         FindUserResponse response = userService.findUser(id);
         return ResponseHandler.success(SuccessCode.RESOURCE_FOUND, response);
     }
 
     @GetMapping("/users")
+    @Operation(summary = "회원 정보 다건 조회", description = "[Deprecated] 모든 회원의 정보를 조회할 수 있도록 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", ref = "Ok"),
+            @ApiResponse(responseCode = "400", ref = "BadRequest"),
+            @ApiResponse(responseCode = "500", ref = "InternalServerError")
+    })
     public ResponseEntity<SuccessResponse> findUsers() {
         List<FindUserResponse> response = userService.findUsers();
         return ResponseHandler.success(SuccessCode.RESOURCE_FOUND, response);
